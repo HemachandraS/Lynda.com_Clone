ba8a39b6382d75f090334fa712dc71aacd9dc9a8
murali0101 <muralimv0101@gmail.com>
2022-04-01 15:07:16 +0530

    port should be dynamic

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 const app = require("./index");
 
 const connect = require("./configs/db");
-
-app.listen(5700, async () => {
+var  port = process.env.PORT || 5700
+app.listen(port, async () => {
   try {
     await connect();
-    console.log("listening port 5700");
+    console.log(`listening port ${port}`);
   } catch (error) {
     console.log(error.message);
   }
