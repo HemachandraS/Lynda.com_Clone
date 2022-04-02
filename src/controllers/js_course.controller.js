29c43ced7ef5faa796bc1ac891848692cf0f09a7
Vishesh Kushwaha <82991580+vishesh11111@users.noreply.github.com>
2022-04-02 10:31:49 +0530

    update data

--- src/controllers/js_course.controller.js
+++ src/controllers/js_course.controller.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 
-const User = require("../models/js_course.model");
+const js_courses = require("../models/js_course.model");
 
 router.get("/", async (req, res) => {
     try {
-      const js_course = await User.find().lean().exec();
+      const js_course = await js_courses.find().lean().exec();
       return res.status(200).send(js_course);
     } catch (error) {
       return res.status(200).send(error.message);
@@ -16,7 +16,7 @@ router.get("/", async (req, res) => {
   
   router.post("/", async (req, res) => {
     try {
-      const js_course = await User.create(req.body)
+      const js_course = await js_courses.create(req.body)
       return res.status(200).send(js_course);
     } catch (error) {
       return res.status(200).send(error.message);
