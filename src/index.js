a6438f8dc86fdd5b8870ef84c2c914d0f8e0985b
HemachandraS <hemachandrahegde2812@gmail.com>
2022-04-02 20:30:43 +0530

    route as been created
    Co-Authored-By: muralimv <66964293+murali0101@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -16,7 +16,11 @@ const html_courseController = require("./controllers/html_course.controller");
 const css_courseController = require("./controllers/css_course.controller");
 const top_pick_courseController = require("./controllers/top_pick_course_controller");
 const js_coursecontroler = require("./controllers/js_course.controller");
-const popular_courseControler = require("./controllers/popular.controller")
+const popular_courseControler = require("./controllers/popular.controller");
+const checkoutController = require("./controllers/checkout.controllers")
+   /////////////////////////////////
+                                            // app.set("view engine", "ejs");
+
 app.post("/register", register);
 app.post("/login", login);
 app.use("/top_pick_course",top_pick_courseController);
@@ -25,6 +29,8 @@ app.use("/css_course", css_courseController);
 app.use("/new_releases", new_releasesController);
 app.use("/js_course", js_coursecontroler);
 app.use("/popular_course", popular_courseControler);
+app.use("/checkout", checkoutController); 
+
 module.exports = app;
 
 // HTML_Data = JSON.stringify(HTML_Data);
