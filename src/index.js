bf08f5279b5fd471d45c71f091197d3eccd7bf3d
Vishesh Kushwaha <82991580+vishesh11111@users.noreply.github.com>
2022-04-02 10:17:41 +0530

    created route for popular and js

--- src/index.js
+++ src/index.js
@@ -15,12 +15,16 @@ const new_releasesController = require("./controllers/new_releases.controller");
 const html_courseController = require("./controllers/html_course.controller");
 const css_courseController = require("./controllers/css_course.controller");
 const top_pick_courseController = require("./controllers/top_pick_course_controller");
+const js_coursecontroler = require("./controllers/js_course.controller");
+const popular_courseControler = require("./controllers/popular.controller")
 app.post("/register", register);
 app.post("/login", login);
 app.use("/top_pick_course",top_pick_courseController);
 app.use("/html_course", html_courseController);
 app.use("/css_course", css_courseController);
 app.use("/new_releases", new_releasesController);
+app.use("/js_course", js_coursecontroler);
+app.use("/popular_course", popular_courseControler);
 module.exports = app;
 
 // HTML_Data = JSON.stringify(HTML_Data);
