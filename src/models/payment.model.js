43157b48c9c5aebc693d4ec9d9d1b0845a6de2ff
HemachandraS <hemachandrahegde2812@gmail.com>
2022-04-02 20:30:08 +0530

    created checkout schema

---
+++ src/models/payment.model.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const paySchema = new mongoose.Schema({
+  first_name: { type: String, required: true },
+  last_name: { type: String, required: true },
+  card_number: { type: String, required: true },
+  exp: { type: String, required: true },
+  sec_code: { type: String, required: true },
+  country: { type: String, required: false },
+  postal: { type: String, required: true },
+  gst: { type: String, required: true },
+},
+{
+  versionKey: false,
+  timestamps: true
+});
+
+const Payment = mongoose.model("paydb", paySchema);
+
+module.exports = Payment;
