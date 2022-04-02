d585fde4b16b106846eeceabdf63e158ce8ac782
Vishesh Kushwaha <82991580+vishesh11111@users.noreply.github.com>
2022-04-02 10:32:45 +0530

    Update popular.controller.js

--- src/controllers/popular.controller.js
+++ src/controllers/popular.controller.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 
-const User = require("../models/popular.model");
+const popular_courses = require("../models/popular.model");
 
 router.get("/", async (req, res) => {
     try {
-      const js_course = await User.find().lean().exec();
+      const js_course = await popular_courses.find().lean().exec();
       return res.status(200).send(js_course);
     } catch (error) {
       return res.status(200).send(error.message);
@@ -16,7 +16,7 @@ router.get("/", async (req, res) => {
   
   router.post("/", async (req, res) => {
     try {
-      const js_course = await User.create(req.body)
+      const js_course = await popular_courses.create(req.body)
       return res.status(200).send(js_course);
     } catch (error) {
       return res.status(200).send(error.message);
